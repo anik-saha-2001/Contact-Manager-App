5a6148c2418830222e5d9c734ac8ea9e6b3e532f
anik-saha-2001 <aniksahamda@gmail.com>
2024-03-07 11:17:33 +0530

    error fixed

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,5 +1,6 @@
 //Don't have to write try catch block, asyncHandler will automatically when error occurs, pass it to the error handler
 const asyncHandler = require("express-async-handler");
+const { ObjectId } = require("mongodb");
 const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
@@ -32,18 +33,37 @@ const createContact = asyncHandler(async (req, res) => {
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async (req, res) => {
-  const contact = await Contact.findById(req.params.id);
+  const contactId = req.params.id;
+
+  if (!isValidObjectId(contactId)) {
+    res.status(400).send({ msg: "Invalid ID" });
+    return;
+  }
+
+  const contact = await Contact.findById(contactId);
+
   if (!contact) {
     res.status(404).send("Contact not found");
+    return;
   }
+
   res.status(200).json(contact);
 });
+
 
 //@desc Update individual contact
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-  const contact = await Contact.findById(req.params.id);
+  const contactId = req.params.id;
+
+  if (!isValidObjectId(contactId)) {
+    res.status(400).send({ msg: "Invalid ID" });
+    return;
+  }
+
+  const contact = await Contact.findById(contactId);
+
   if (!contact) {
     res.status(404).send("Contact not found");
   }
@@ -60,14 +80,36 @@ const updateContact = asyncHandler(async (req, res) => {
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-  const contact = await Contact.findById(req.params.id);
-  if (!contact) {
-    res.status(404).send("Contact not found");
+  const contactId = req.params.id;
+
+  if (!isValidObjectId(contactId)) {
+    res.status(400).send("Invalid ID");
+    return;
   }
 
-  await Contact.remove();
-  res.status(200).json(contact);
+  const contact = await Contact.findById(contactId);
+
+  if (!contact) {
+    res.status(404).send("Contact not found");
+    return;
+  }
+
+  // Use findByIdAndDelete instead of findByIdAndRemove
+  const deletedContact = await Contact.findByIdAndDelete(contactId);
+
+  if (!deletedContact) {
+    // If the contact was not found, it means it has already been deleted
+    res.status(200).json({ message: "Contact already deleted" });
+  } else {
+    // Respond with the deleted contact information
+    res.status(200).json(deletedContact);
+  }
 });
+
+// Checks if the ObjectId is valid or not
+function isValidObjectId(id) {
+  return ObjectId.isValid(id);
+}
 
 module.exports = {
   getContacts,
